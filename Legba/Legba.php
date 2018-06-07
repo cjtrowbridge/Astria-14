a006707a9c89ce7def00dafcf5b40ae619e4d140
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-06-07 12:21:20 -0700

    Create Legba.php

---
+++ Legba/Legba.php
@@ -0,0 +1,47 @@
+<?php
+
+class Legba{
+  
+  
+  
+  //Native class primitives
+  function __construct(){
+    
+  }
+  function __destruct(){
+    
+  }
+  function __call(){
+    //TODO Include graceful error handling
+  }
+  
+  
+  //Accessors
+  public function Config($File, $Key, $Value = '9BDUo49XFN54CMaIfQOb4Cd2PB8BmZwZQ3ktFhXj1i9Tz8u7cTiKvRt526Tzo5QQ'){
+    //Obfuscated default parameter is designed to allow passing of values like null, false, etc. 
+    
+    return false;
+  }
+  public function Event($Name, $Callback = false){
+    //Hook a callback onto an event name or trigger the callbacks associated with the event name.
+    
+    return false;
+  }
+  public function Load($Directory = false){
+    //Load all plugins in the specified directory
+    
+    return false;
+  }
+  public function LoggedIn(){
+    //Check whether a user is currently logged in and authenticated. Return user or false.
+    
+    return false;
+  }
+  public function RequireSSL(){
+    //If the current path is not an SSL path, then redirect to an SSL version of the current path.
+    
+    return false;
+  }
+  
+}
+  
