1c0da64e9151d53eb46c05cfe0ae0829d0a96f60
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-06-07 12:55:15 -0700

    Update Legba.php

--- Legba/Legba.php
+++ Legba/Legba.php
@@ -2,10 +2,30 @@
 
 class Legba{
   
-  
+  private $ThreadID = false;
+  private $Debug    = false;
+  private $Events   = false;
   
   //Native class primitives
   function __construct(){
+    //Create a new id for this thread.
+    $this->$ThreadID = sha256(uniqid(true));
+    
+    //Set up initial debug array.
+    $this->$Debug = array(
+      0=>array(
+        'Description'=> 'Legba Constructor',
+        'RAM'        => (memory_get_usage()/1000000),
+        'Delta-RAM'  => 0,
+        'Time'       => round(microtime(true),4),
+        'Delta-Time' => 0
+      )
+    );
+    
+    //Create initial empty events list.
+    $this->$Events = array(
+      
+    );
     
   }
   function __destruct(){
@@ -13,18 +33,74 @@ class Legba{
   }
   function __call($Name, $Arguments){
     //TODO Include graceful error handling
+    die('Legba: Unknow function "'.$Name.'" called.');
   }
   
   
-  //Accessors
+  //Static Functions
+  public static function pd($Input){
+    echo '<pre>';
+    var_dumo($Input);
+    echo '</pre>';
+  }
+  public static function sha256($Input){
+    return hash('sha256', $Input);
+  }
+  public static function sha512($Input){
+    return hash('sha512', $Input);
+  }
+  
+  
+  //Accessor/Mutator Functions
   public function Config($File, $Key, $Value = '9BDUo49XFN54CMaIfQOb4Cd2PB8BmZwZQ3ktFhXj1i9Tz8u7cTiKvRt526Tzo5QQ'){
-    //Obfuscated default parameter is designed to allow passing of values like null, false, etc. 
+    //Obfuscated default parameter is designed to allow passing of values like null, false, etc.
+    
+    if($Value == '9BDUo49XFN54CMaIfQOb4Cd2PB8BmZwZQ3ktFhXj1i9Tz8u7cTiKvRt526Tzo5QQ'){
+      //Search for key's current value in specified file. Return it or null.
+      
+      
+    }else{
+      //Set the given key to the given value in the given file. Return true or false regarding ability to save.
+      
+      
+    }
     
     return false;
   }
   public function Event($Name, $Callback = false){
     //Hook a callback onto an event name or trigger the callbacks associated with the event name.
     
+    if($Callback == false){
+      //Trigger the callbacks hooked to a particular event name
+      if(isset($this->$Events[$Name])){
+        foreach($this->$Events[$Name] as $Callback){
+          /* Note that the callback is evaluated, and as such can be any php script, but must be syntactically complete. ie: "foo();" */
+          try{
+            eval($Callback);
+          }catch(Exception $e){
+            /* TODO permissions and config to permit this
+            Event('Event Exception');
+            echo '<p><b>EVENT THREW EXCEPTION</b></p>';
+            pd($e);
+            */
+          }
+        }
+      }
+    }else{
+      //Hook a callback onto an event name
+      if(is_string($Name)){
+        /* If this event doesn't already exist, create it. */
+        if(
+          (!(isset($this->$Events[$Name])))
+        ){
+          $this->$Events[$Name]=array();
+        }
+        /* Add the callback to the array for this event */
+        $this->$Events[$Name][]=$Callback;
+      }else{
+        fail('<h1>Event Description Must Be A String;</h1><pre>'.var_export($EventDescription,true).'</pre>');
+      }
+    }
     return false;
   }
   public function Load($Directory = false){
