9992925e829ebda600136360c6b344aa6cd091e8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-06-09 13:38:53 -0700

    Update Architect.php

--- Architect/Architect.php
+++ Architect/Architect.php
@@ -3,7 +3,7 @@
   
   Architect 2.0
   
-  Architect is an Astria module which allows admins to manage users, groups, pages, routes, database connections, and configuration settings via a web interface.
+  Architect is an Astria module which allows admins to manage users, groups, pages, routes, database connections, files, and configuration settings via a web interface.
   
 */
 class Architect{
