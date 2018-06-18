e4532cf354d2f0c2cb836d73b5089cc5ef9be2db
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-06-17 18:44:46 -0700

    Update Legba.php

--- Legba/Legba.php
+++ Legba/Legba.php
@@ -40,9 +40,9 @@ class Legba{
     
     //Figure out the route and set a global variable in case htaccess is not available.
     if(!(isset($_GET['route']))){
+      //If the route is not set via superglobal, then parse it from the REQUEST_URI
       $_GET['route'] = $_SERVER['REQUEST_URI'];
     }
-    
     //Remove any extra slashes in the request
     $_GET['route'] = trim($_GET['route'], '/');
     //If there is a question mark, truncate the url we are parsing at that point.
@@ -50,8 +50,6 @@ class Legba{
     $_GET['route'] = $Temp[0];
     //Make sure there is exactly one trailing slash in the route
     if(!(substr($_GET['route'], -1)=='/')){$_GET['route'].='/';}
-    //Set the superglobal variable $_GET['route'] to this route
-    
     $RequestSegments = explode('/', $_GET['route']);
     $Route = array();
     foreach($RequestSegments as $RequestSegment){
